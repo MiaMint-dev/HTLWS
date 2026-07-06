51f3dccc07baa2e953bfb2f0febfc929255c05b4
MiaMint <283266309+MiaMint-dev@users.noreply.github.com>
2026-07-06 19:28:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Seconds = document.getElementById('seconds');
 const ledBoard = document.getElementById('board-text');
 
 if (Days && Hours && Minutes && Seconds && ledBoard) {
-const targetDate = new Date("July 6 2026 16:00:00").getTime();
+const targetDate = new Date("July 10 2026 16:00:00").getTime();
 
 function timer () {
   const currentDate = new Date().getTime();
